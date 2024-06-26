2afdbe36359998cfdc5c98ed14fb530b1e325697
Augustin <a.lefevre89@gmail.com>
2024-06-26 14:49:08 +0200

    add service aibot

---
+++ aibot-service/src/main/java/com/yarnyard/aibot_service/AibotServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yarnyard.aibot_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AibotServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AibotServiceApplication.class, args);
+	}
+
+}
---
+++ aibot-service/src/test/java/com/yarnyard/aibot_service/AibotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yarnyard.aibot_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AibotServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
