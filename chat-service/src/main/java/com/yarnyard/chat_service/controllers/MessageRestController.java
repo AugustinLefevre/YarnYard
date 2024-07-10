b31e213fc3a89638712a2d2b394bb0d3bb958b58
Augustin <a.lefevre89@gmail.com>
2024-07-10 17:10:43 +0200

    update cors configuration

--- chat-service/src/main/java/com/yarnyard/chat_service/controllers/MessageRestController.java
+++ chat-service/src/main/java/com/yarnyard/chat_service/controllers/MessageRestController.java
@@ -3,7 +3,6 @@ package com.yarnyard.chat_service.controllers;
 import com.yarnyard.chat_service.entities.Message;
 import com.yarnyard.chat_service.services.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +11,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/message")
-@CrossOrigin(origins = "*")
 public class MessageRestController {
     @Autowired
     private MessageService service;
